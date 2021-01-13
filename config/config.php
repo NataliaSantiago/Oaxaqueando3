09a18680550a3873f240fb7438bee020949de5ea
ObedG <65927298+ObedG@users.noreply.github.com>
2021-01-12 19:43:08 -0600

    Archivos

---
+++ config/config.php
@@ -0,0 +1,8 @@
+<?php
+    define('URL', 'http://localhost/mvc2/');
+    define('HOST', 'bdyzcfehrcomfhksbhmx-mysql.services.clever-cloud.com');
+    define('DB', 'bdyzcfehrcomfhksbhmx');
+    define('USER', 'up1bqfux0eibfreq');
+    define('PASSWORD', 'Oe4HhihpfWtMz8oLw7DZ');
+    define('CHARSET', 'utf8mb4');
+?>
