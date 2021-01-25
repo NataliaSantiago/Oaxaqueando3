aea4af7dee377c52e5e00d9315605f5bd9430f91
ObedG <65927298+ObedG@users.noreply.github.com>
2021-01-25 14:03:37 -0600

    Update index.php

--- views/lista/index.php
+++ views/lista/index.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width", initial-scale="1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Wishlist</title>
+    <title>Carrito de compras</title>
     <link  rel = "stylesheet" href ="public/css/estilacho.css">
 
 </head>
@@ -17,7 +17,7 @@
      ?>
     <div id="contenedor">
    
-    <h1 class="center">Wishlist</h1>
+    <h1 class="center">Carrito de compras</h1>
 
     <h1>Bienvenido <?php echo $_SESSION['user'];  ?></h1>
     <div id=respuesta class="center"></div>  
