ad9d7c2c010d43d3942e0eecb47eabf3503658da
ObedG <65927298+ObedG@users.noreply.github.com>
2021-05-08 02:05:03 -0500

    Despliegue en heroku

--- config/config.php
+++ config/config.php
@@ -1,8 +1,8 @@
 <?php
-    define('URL', 'http://app-a048003f-c2d8-447d-ae4d-61eaaa1eb11e.cleverapps.io/');
-    define('HOST', 'bdyzcfehrcomfhksbhmx-mysql.services.clever-cloud.com');
-    define('DB', 'bdyzcfehrcomfhksbhmx');
-    define('USER', 'up1bqfux0eibfreq');
-    define('PASSWORD', 'Oe4HhihpfWtMz8oLw7DZ');
+    define('URL', 'https://oaxaqueandoapp.herokuapp.com/');
+    define('HOST', 'bttlrstie3bco4jbshqr-mysql.services.clever-cloud.com');
+    define('DB', 'bttlrstie3bco4jbshqr');
+    define('USER', 'uj4csglerc2av4og');
+    define('PASSWORD', 'gYS4I9XHBlpTdnLUMUPt');
     define('CHARSET', 'utf8mb4');
 ?>
--- connect_db.php
+++ connect_db.php
@@ -1,9 +1,9 @@
 <?php
 
-define('USER', 'up1bqfux0eibfreq');
-define('PASSWORD', 'Oe4HhihpfWtMz8oLw7DZ');
-define('HOST', 'bdyzcfehrcomfhksbhmx-mysql.services.clever-cloud.com');
-define('DATABASE', 'bdyzcfehrcomfhksbhmx');
+define('USER', 'uuj4csglerc2av4og');
+define('PASSWORD', 'gYS4I9XHBlpTdnLUMUPt');
+define('HOST', 'bttlrstie3bco4jbshqr-mysql.services.clever-cloud.com');
+define('DATABASE', 'bttlrstie3bco4jbshqr');
     
     
  
